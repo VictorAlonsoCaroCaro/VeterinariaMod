44102e83d705b7d04e55c7986504fc711333ea29
Juan Carlos Zambrano <jczambrano@celsia.com>
2022-09-10 17:11:08 -0500

    Ajuste de javascript con ajax

--- HostiEnCasa.App/HospiEnCasa.App.WebApp/Pages/GestionPersonas/List.cshtml.cs
+++ HostiEnCasa.App/HospiEnCasa.App.WebApp/Pages/GestionPersonas/List.cshtml.cs
@@ -13,11 +13,52 @@ namespace HospiEnCasa.App.WebApp
     {
         private IPersonaRepository _personaRepository = new PersonaRepository( new HostiEnCasa.App.Persistencia.AppContext() );
         public List<Persona> listadoPersonas { get; set;}
+        public string mensaje;
 
         public void OnGet()
         {
             listadoPersonas = new List<Persona>();
             listadoPersonas = _personaRepository.ObtenerTodo();
+            if(ViewData["mensaje"] != null)
+                mensaje = ViewData["mensaje"].ToString();
+            else
+                mensaje = "";
         }
+
+        public IActionResult OnPostUpdate()
+        {
+            return Content("Se ejecuto el consumo del metodo Update via ajax");
+        }
+
+        public IActionResult OnPostUpdateJson([FromBody]Persona persona)
+        {
+            
+            var personaResult = _personaRepository.Buscar( persona.Id );
+            var mensaje = "";
+
+            if( personaResult != null){
+
+                personaResult.Nombre = persona.Nombre;
+                personaResult.Apellidos = persona.Apellidos;
+                personaResult.NumeroTelefono = persona.NumeroTelefono;
+                //personaResult.Genero = (persona.Genero == "0" ? Genero.Femenino : Genero.Masculino);
+            
+                var result = _personaRepository.Update(personaResult);
+
+                if( result > 0){
+                    mensaje = "Se actualizo correctamente";
+                }else{
+                    mensaje = "No se pudo actualizar";
+                }
+
+            }else{
+                mensaje = "La persona a actualizar no existe";
+            }
+
+            //return new JsonResult(persona);
+
+            return Content(mensaje);
+
+        } 
     }
 }
--- HostiEnCasa.App/HospiEnCasa.App.WebApp/wwwroot/js/personaList.js
+++ HostiEnCasa.App/HospiEnCasa.App.WebApp/wwwroot/js/personaList.js
@@ -1,17 +1,85 @@
 $().ready(function(){
     
+    /*
     $("#eliminar").click(function(){
         $('#modalEliminar').modal('show');
     });
+    
+    $("#editar").click(function(){
+        $('#modalEditar').modal('show');
+    });
+    */
 
-    $("#actualizar").click(function(){
+    $(document).on('click', '#tablePersonas tbody tr td a.btn.btn-secondary', function(){
+        $('#modalEliminar').modal('show');
+    });
 
-        //Colocar datos seleccionados al modal
-        $("#nombre").val("Juan Carlos");
-        $("#apellido").val("Zambrano");
-        $("#telefono").val("31298765432");
+    $(document).on('click', '#tablePersonas tbody tr td a.btn.btn-primary', function(){
+        $(this).parent().parent().find('td').each(function(index){
+            switch(index){
+                case 0:
+                    $('#Id').val($(this).text());
+                    break;
+                case 1:
+                    $('#nombre').val($(this).text());
+                    break;
+                case 2:
+                    $('#apellido').val($(this).text());
+                    break;
+                case 3:
+                    $('#telefono').val($(this).text());
+                    break;                  
+            }
+        });
 
         $('#modalEditar').modal('show');
+
+    });
+
+    $("#btnActualizar").click(function(){
+        //Validar datos
+
+        /* Enviar petición AJAX datos crudos*/
+        /*
+        $.ajax({
+            type: "POST",
+            url: "/GestionPersonas/List?handler=Update",
+            contentType: "application/html; charset=utf-8",
+            dataType: "html",
+            headers: {
+                "RequestVerificationToken": $('input:hidden[name="__RequestVerificationToken"]').val()
+            },
+            data: $('#formEditar').serialize(),
+        })
+        .done(function (result) {
+            alert(result);
+        })
+        .fail(function (error) {
+            alert(error);
+        });
+        */
+
+        /* Enviar petición AJAX datos JSON */
+        var persona = { "Id": $("#Id").val(), "Nombre": $("#nombre").val(), "Apellidos": $("#apellido").val(), "NumeroTelefono": $("#telefono").val() };
+
+        $.ajax({
+            type: "POST",
+            url: "/GestionPersonas/List?handler=UpdateJson",
+            contentType: "application/json; charset=utf-8",
+            dataType: "html",
+            headers: {
+                "RequestVerificationToken": $('input:hidden[name="__RequestVerificationToken"]').val()
+            },
+            data: JSON.stringify(persona),
+        })
+        .done(function (result) {
+            alert(result);
+            location.reload();
+        })
+        .fail(function (error) {
+            alert(error);
+        });
+
     });
 
 });
